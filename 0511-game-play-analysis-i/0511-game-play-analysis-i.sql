5d2fcb3dbd043f08f5fc09e78ee17f33350fe4e1
Jaehyeong Kwon <89461624+Stillav@users.noreply.github.com>
2023-01-06 18:14:54 +0900

    Time: 787 ms (44.55%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -2,11 +2,12 @@
 
 SELECT DISTINCT
   A.player_id,
-  FIRST_VALUE(A.event_date) OVER (
+  LAST_VALUE(A.event_date) OVER (
     PARTITION BY
       A.player_id
     ORDER BY
-      A.event_date
+      A.event_date DESC RANGE BETWEEN UNBOUNDED PRECEDING
+      AND UNBOUNDED FOLLOWING
   ) AS first_login
 FROM
-  Activity A;
+  Activity A
