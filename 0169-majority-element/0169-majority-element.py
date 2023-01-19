78f3004b72477de66d2472d5e1ea843577d7b360
Jaehyeong Kwon <89461624+Stillav@users.noreply.github.com>
2023-01-19 17:13:00 +0900

    Time: 225 ms (40.99%), Space: 15.4 MB (82.92%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        result = dict()
+        if len(nums) == 1:
+            return nums[0]
+        
+        else:
+            for i in nums:
+                if str(i) in result:
+                    result[str(i)] += 1
+                    if result[str(i)] > len(nums)//2:
+                        return i
+                else:
+                    result[str(i)] = 1
+
+        
+                
+            
