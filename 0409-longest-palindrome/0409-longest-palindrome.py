07a1c755a30c5e6a3a2a160c0b399081680437fe
Jaehyeong Kwon <89461624+Stillav@users.noreply.github.com>
2023-09-11 23:03:30 +0900

    Time: 78 ms (6.27%), Space: 16.4 MB (42.01%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,14 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        spell_list = list(s)
-        spell_dict = {spell: spell_list.count(spell) for spell in spell_list}
-        answer = 0
-        one_odd = False 
-        for key in spell_dict:
-            if spell_dict[key] % 2 != 0 and not one_odd:
-                one_odd = True
-                answer += 1
-                
-            answer += spell_dict[key] if spell_dict[key] % 2 == 0 else spell_dict[key] - 1
-        
-        return answer 
+        ss = set()
+        for i in s:
+            if i not in ss: 
+                ss.add(i)
+                print(ss)
+            else: 
+                ss.remove(i)
+                print(ss)
+        return len(s) - len(ss) + 1 if len(ss) != 0 else len(s) 
