af825ba23db6e049f650ac1acc62afa67321e17a
Jaehyeong Kwon <89461624+Stillav@users.noreply.github.com>
2024-01-23 21:21:21 +0900

    Time: 48 ms (68.50%), Space: 17.6 MB (98.16%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.isbal = True 
+        
+        def depth(node):
+            if not node:
+                return 0
+            
+            
+            
+            left_depth = depth(node.left)
+            right_depth = depth(node.right)
+            if left_depth is None or right_depth is None:
+                return None 
+            
+            if abs(left_depth - right_depth) > 1:
+                self.isbal = False
+                return None
+            else:
+                return max(left_depth, right_depth) + 1 
+            
+        depth(root)
+        
+        return self.isbal
